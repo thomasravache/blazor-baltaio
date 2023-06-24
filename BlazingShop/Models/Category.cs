6caea8ee70cc677e632e00173d32b6d606b7cf8a
Thomas Ravache <thomasravache31@gmail.com>
2023-06-24 19:42:23 -0300

    feat: Adicionando o Entity Framework

--- BlazingShop/Models/Category.cs
+++ BlazingShop/Models/Category.cs
@@ -1,7 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BlazingShop.Models;
 
 public class Category
 {
+    [Key]
+    [Required(ErrorMessage = "O Id é obrigatório")]
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Título é obrigatório")]
+    [MaxLength(50, ErrorMessage = "Título deve ter no máximo 50 caracteres")]
+    [MinLength(5, ErrorMessage = "Título deve ter no mínimo 5 caracteres")]
+    public string Title { get; set; } = string.Empty;
+
+    public List<Product> Products { get; set; } = new();
 }
