373d253b9bc133061a1ba7bc18042ba31a36eebd
Thomas Ravache <thomasravache31@gmail.com>
2023-07-02 19:24:43 -0300

    feat: Validando Formulários

--- BlazingShop/Models/Product.cs
+++ BlazingShop/Models/Product.cs
@@ -15,7 +15,7 @@ public class Product
 
     [Required(ErrorMessage = "Preço é obrigatório")]
     [DataType(DataType.Currency)]
-    [Range(0, 9999, ErrorMessage = "Preço deve estar entre 0 e 9999")]
+    [Range(1, 9999, ErrorMessage = "Preço deve estar entre 1 e 9999")]
     public decimal Price { get; set; }
 
     [Required]
